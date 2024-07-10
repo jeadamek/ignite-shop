1f8e2466b5e7aeacd93f72fe3b2131bd95708cdf
Jessica Adamek <jessica.g.adamek@gmail.com>
2024-07-09 21:16:32 -0300

    :lipstick: global styles

---
+++ src/styles/index.ts
@@ -0,0 +1,26 @@
+import { createStitches } from "@stitches/react"; 
+
+export const {
+  config,
+  styled,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+} = createStitches({
+  theme: {
+    colors: {
+      white: '#FFF',
+
+      gray900: '#121214',
+      gray800: '#202024',
+      gray300: '#C6C6CC',
+      gray100: '#E1E1E6',
+
+      green500: '#008775F',
+      green300: '#00B37E',
+    }
+  }
+})
+
