12502b6a1e43916f3e020f7c50a3ea5b9ff1e9b8
Jessica Adamek <jessica.g.adamek@gmail.com>
2024-07-10 00:19:27 -0300

    :lipstick: carousel style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Image from "next/image";
+
+import { useKeenSlider } from  'keen-slider/react';
+
 import { HomeContainer, Product } from "../styles/pages/home";
 
 import shirt1 from '../assets/camisetas/Shirt-1.png';
@@ -7,9 +10,16 @@ import shirt3 from '../assets/camisetas/Shirt-3.png';
 import shirt4 from '../assets/camisetas/Shirt-4.png';
 
 export default function Home() {
+  const [ sliderRef ] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={shirt1} width={530} height={480} alt="" />
 
         <footer>
@@ -18,8 +28,25 @@ export default function Home() {
         </footer>
       </ Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={shirt2} width={530} height={480} alt="" />
+
+        <footer>
+          <strong> Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </ Product>
+      
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} width={530} height={480} alt="" />
+
+        <footer>
+          <strong> Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </ Product>
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} width={530} height={480} alt="" />
 
         <footer>
           <strong> Camiseta X</strong>
