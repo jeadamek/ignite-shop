4a65a963c41099a4f4610189aea978eee9069785
Jessica Adamek <jessica.g.adamek@gmail.com>
2024-07-28 19:45:46 -0300

    connect to stripe checkout session

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { useState } from 'react';
 import { ImageContainer, ProductContainer, ProductDetails } from '@/src/styles/pages/product';
 import { useRouter } from 'next/router';
 import { GetStaticPaths, GetStaticProps } from 'next';
@@ -12,14 +14,34 @@ interface ProductProps {
     imageUrl: string;
     price: string;
     description: string;
+    defaultPriceId: string;
   }
 }
 
 export default function Product({ product }: ProductProps) {
   const { isFallback } = useRouter();
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false);
 
   if (isFallback) {
     return <p>Loading...</p>
+  }
+
+  async function handleBuyProduct() {
+    try {
+      setIsCreatingCheckoutSession(true);
+
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId,
+      })
+
+      const { checkoutUrl } = response.data;
+
+      window.location.href = checkoutUrl
+    } catch (err) {
+      // Conectar com uma ferramenta de observabilidade (datadog / Sentry)
+      setIsCreatingCheckoutSession(false);
+      alert('Falha ao redirecionar ao checkout!')
+    }
   }
 
   return (
@@ -33,7 +55,12 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
       
-        <button>Comprar Agora</button>
+        <button 
+          onClick={handleBuyProduct}
+          disabled={isCreatingCheckoutSession}
+        >
+          Comprar Agora
+        </button>
       </ProductDetails>
     </ProductContainer>
   )
@@ -68,6 +95,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
           currency: 'BRL',
         }).format((price.unit_amount || 0) / 100),
         description: product.description,
+        defaultPriceId: price.id,
       }
     },
     revalidate: 60 * 60,
